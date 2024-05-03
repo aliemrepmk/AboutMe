cacde10968be41f4d7b9346ede7e78bccb73af38
aliemrepmk <aliemrepamuk0@gmail.com>
2024-05-03 15:36:03 +0300

    03.05 class activity

---
+++ InclassActivities8/AltekinPamukActivity8.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $("#birthday").datepicker();
+});
+
+
+$(function() {
+    var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+    ];
+
+    $("#language").autocomplete({
+        source: availableTags
+    });
+});
